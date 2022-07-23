744e5de0eaa76a66730f3030c6be35a1dda33ad9
John Valai <git@jvk.to>
2022-07-23 15:24:32 +0100

    chore: add more code comments

---
+++ TODO.md
@@ -0,0 +1,19 @@
+# Project
+
+jv-k readme.md generator
+
+<em>[TODO.md spec & Kanban Board](https://bit.ly/3fCwKfM)</em>
+
+### Todo
+
+- [ ] Add More tests  
+
+### In Progress
+
+
+
+### Done ✓
+
+- [x] Add JSDoc  
+- [x] Add error handling  
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-// index.js
+/**
+ * @fileoverview Main entrypoint for generation of README.md
+ * @author John Valai <git@jvk.to>
+ */
+
 import { load as configLoad } from 'node-yaml-config';
 import SkillSet from './lib/skillset.js';
 
 const config = configLoad('./src/config.yaml');
-const mySkills = await new SkillSet(config);
-await mySkills.render();
+const mySkills = new SkillSet(config);
+await mySkills.renderReadme();
